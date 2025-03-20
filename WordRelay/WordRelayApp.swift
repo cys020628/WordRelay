fae8b4c4738aa72a84fb974931e70d26213582b9
youngseoc <cys020628@naver.com>
2025-03-20 10:53:13 +0900

    Initial Commit

---
+++ WordRelay/WordRelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordRelayApp.swift
+//  WordRelay
+//
+//  Created by 영석 on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WordRelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordRelayTests/WordRelayTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordRelayTests.swift
+//  WordRelayTests
+//
+//  Created by 영석 on 3/20/25.
+//
+
+import Testing
+@testable import WordRelay
+
+struct WordRelayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
